d20e78be8d87751abf75acb24a10a0dae00367e6
camilo <camilob270@gmail.com>
2020-08-23 16:18:56 -0400

    added encrypted service account file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t camilojimenez/multi-client:latest -t camilojimenez/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t camilojimenez/multi-server:latest -t camilojimenez/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t camilojimenez/multi-worker:latest -t camilojimenez/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t camilojimenezf/multi-client:latest -t camilojimenezf/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t camilojimenezf/multi-server:latest -t camilojimenezf/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t camilojimenezf/multi-worker:latest -t camilojimenezf/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push camilojimenez/multi-client:latest
-docker push camilojimenez/multi-server:latest
-docker push camilojimenez/multi-worker:latest
+docker push camilojimenezf/multi-client:latest
+docker push camilojimenezf/multi-server:latest
+docker push camilojimenezf/multi-worker:latest
 
-docker push camilojimenez/multi-client:$SHA
-docker push camilojimenez/multi-server:$SHA
-docker push camilojimenez/multi-worker:$SHA
+docker push camilojimenezf/multi-client:$SHA
+docker push camilojimenezf/multi-server:$SHA
+docker push camilojimenezf/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=camilojimenez/multi-client:$SHA
-kubectl set image deployments/server-deployment server=camilojimenez/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=camilojimenez/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=camilojimenezf/multi-server:$SHA
+kubectl set image deployments/client-deployment client=camilojimenezf/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=camilojimenezf/multi-worker:$SHA
