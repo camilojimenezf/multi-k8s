b16b552f93b4837c1b6974fd2a5c4027499de22e
camilo <camilob270@gmail.com>
2020-08-23 08:36:03 -0400

    k8s config

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            FIB CALCULATOR
+            FIB CALCULATOR V2
           </p>
           <a
             className="App-link"
